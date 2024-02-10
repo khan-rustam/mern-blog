678ac711607ef73d35f420e3c09b0f30d5405de6
khan-rustam <khanrustam2809@gmail.com>
2024-02-10 18:21:21 +0530

    connection to mongo db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-const PORT = 3000;
+//connection to mongoDB database --------------
+mongoose
+  .connect(
+      "mongodb+srv://mern-blog:mern-blog@cluster0.gxs1bat.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected successfully to MongoDB"))
+  .catch((err) => console.log(err, "Failed to connect to MongoDB"));
 
+//Server runnning on PORT - 3000 --------------
+const PORT = 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}!!`));
