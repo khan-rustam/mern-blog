58b0c07c31006dfd95b4e2f38e1750e90163f23c
khan-rustam <khanrustam2809@gmail.com>
2024-02-10 18:26:22 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+}, {timestamps: true});
+const User = mongoose.model("User", schema);
+
+export default User;
