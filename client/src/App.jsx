641c22af63200c2d21507ac03289b491aaaa4056
khan-rustam <khanrustam2809@gmail.com>
2024-02-16 14:17:28 +0530

    create & added private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Projects from "./pages/Projects";
 import NotFound from "./components/NotFound";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
@@ -20,7 +21,9 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
