100b55a85a83fce7f59632ed3d14ee3269240c8f
khan-rustam <khanrustam2809@gmail.com>
2024-02-20 18:36:49 +0530

    fix user update api route

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from "express";
+import { updateUser } from "../controller/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
-
-
-
+router.put("/update/:userId", verifyToken, updateUser);
 
 export default router;
