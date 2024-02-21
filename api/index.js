b44184cb434a53ff236d3ddef6f72d825109551a
khan-rustam <khanrustam2809@gmail.com>
2024-02-21 22:58:37 +0530

    create post api route

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.routes.js";
-import authRouter from "./routes/auth.routes.js";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import postRouter from './routes/post.routes.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 //connection to mongoDB database --------------
 mongoose
@@ -20,14 +21,16 @@ mongoose
   .then(() => console.log('Connected successfully to MongoDB'))
   .catch((err) => console.log(err, 'Failed to connect to MongoDB'));
 
-app.use("/api/auth", authRouter);
-app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/post', postRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({ success: false, message: message });
 });
 
-//Server runnning on PORT - 3000 --------------
+//Server running on PORT - 3000 --------------
 const PORT = 3000;
 app.listen(PORT, () => console.log(`listening on ${PORT}!!`));
