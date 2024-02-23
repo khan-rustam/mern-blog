93d84b4ef895d8b37c0bb48b1e5c2a88dd7e8704
khan-rustam <khanrustam2809@gmail.com>
2024-02-23 22:57:59 +0530

    create get users api route

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -1,15 +1,15 @@
-import User from "../models/user.model.js";
-import { errorHandler } from "../utils/error.js";
-import bcryptjs from "bcryptjs";
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs';
 
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, "You are not allowed to update this user"));
+    return next(errorHandler(403, 'You are not allowed to update this user'));
   }
 
   if (req.body.password) {
     if (req.body.password.length < 6) {
-      return next(errorHandler(400, "Password must be at least 6 characters"));
+      return next(errorHandler(400, 'Password must be at least 6 characters'));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
@@ -17,14 +17,14 @@ export const updateUser = async (req, res, next) => {
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
-        errorHandler(400, "Username must be between 7 and 20 characters")
+        errorHandler(400, 'Username must be between 7 and 20 characters')
       );
     }
-    if (req.body.username.includes(" ")) {
-      return next(errorHandler(400, "Username cannot contain spaces"));
+    if (req.body.username.includes(' ')) {
+      return next(errorHandler(400, 'Username cannot contain spaces'));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(errorHandler(400, "Username must be lowercase"));
+      return next(errorHandler(400, 'Username must be lowercase'));
     }
   }
 
@@ -34,7 +34,7 @@ export const updateUser = async (req, res, next) => {
 
   if (!validUsername) {
     return next(
-      errorHandler(400, "Username can only contain letters and numbers")
+      errorHandler(400, 'Username can only contain letters and numbers')
     );
   }
 
@@ -60,21 +60,65 @@ export const updateUser = async (req, res, next) => {
 
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, "You are not allowed to delete this user"));
+    return next(errorHandler(403, 'You are not allowed to delete this user'));
   }
 
   try {
     await User.findByIdAndDelete(req.params.userId);
-    res.status(200).json("User deleted successfully");
+    res.status(200).json('User deleted successfully');
   } catch (error) {
     next(error);
   }
 };
 
 export const signOut = async (req, res, next) => {
-
   try {
-    res.clearCookie('access_token').status(200).json("Sign Out successfully");
+    res.clearCookie('access_token').status(200).json('Sign Out successfully');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, 'You are not allowed to access this route'));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const shortDirection = req.query.order === 'asc' ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: shortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const userWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: {
+        $gte: oneMonthAgo,
+      },
+    });
+
+    res.status(200).json({
+      users: userWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
   } catch (error) {
     next(error);
   }
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,14 +3,18 @@ import {
   updateUser,
   deleteUser,
   signOut,
-} from "../controller/user.controller.js";
+  getUsers,
+} from '../controller/user.controller.js';
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 router.put("/update/:userId", verifyToken, updateUser);
 
 router.delete("/delete/:userId", verifyToken, deleteUser);
+
 router.post("/signout", signOut);
+
+router.get('/get-users', verifyToken, getUsers)
 
 
 export default router;
