0734602a1ab20e1f0f4f606787b44299dd41e6c1
khan-rustam <khanrustam2809@gmail.com>
2024-02-25 01:07:19 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,26 +6,32 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import postRouter from './routes/post.routes.js';
 import commentRouter from './routes/comment.routes.js';
+import path from 'path';
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+dotenv.config();
 
+const URI = process.env.MONGO_DB_URI;
 //connection to mongoDB database --------------
 mongoose
-  .connect(
-    'mongodb+srv://mern-blog:mern-blog@cluster0.selnfqf.mongodb.net/?retryWrites=true&w=majority',
-    { dbName: 'mern-blog' }
-    // "mongodb+srv://mern-blog:mern-blog@cluster0.selnfqf.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(URI, { dbName: 'mern-blog' })
   .then(() => console.log('Connected successfully to MongoDB'))
   .catch((err) => console.log(err, 'Failed to connect to MongoDB'));
+
+const __dirname = path.resolve();
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 app.use('/api/post', postRouter);
 app.use('/api/comment', commentRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
